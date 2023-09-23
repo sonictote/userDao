592d480a745eb2c7779d461709cd7700e677eeed
Andres Martinez <sonictote@gmail.com>
2023-09-23 18:32:48 +0200

    Creacion del proyecto y puesta en funcionamiento de la bbdd H2

---
+++ src/main/java/com/sonictote/userDAO/UserDaoApplication.java
@@ -0,0 +1,13 @@
+ package com.sonictote.userDAO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDaoApplication.class, args);
+	}
+
+}
